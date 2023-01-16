026b41f1c530e30d78b9b02a8a0babe011984563
Alex Network <alex199587@hotmail.com>
2023-01-15 22:32:01 -0600

    Create CheckOutput.sh

---
+++ CheckOutput.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+clear; clear;
+if test -f /root/RD_DEMO_FINAL/output.txt; then
+	rm -rf /root/RD_DEMO_FINAL/output.txt
+fi
+day=$(date +%A)
+month=$(date +%B)
+day_month=$(date +%d)
+year=$(date +%Y)
+hour=$(date +%r |sed 's/:/ /g' |awk '{print$1":"$2,$4}')
+
+echo " "
+echo -e "\t\t\e[44m $day $day_month $month $year $hour \e[0m"
+echo " "
+echo -e "\t\e[36m   ----- Check Output from Palindrome, RD_DEMO -----\e[0m"
+echo " "
+
+function owner(){
+	echo " "
+	echo -e "\e[36m Powered by @Alejandro Arevalo\e[0m"
+	echo " "
+}
+
+function output(){
+	read -p "How many time do you want to hit the API: " tms
+	read -p "Please enter a word: " word
+	i=1
+	while [ $i -le $tms ]
+		do
+			curl localhost:8181/palindrome/$word >> output.txt
+			echo " " >> output.txt
+			echo "---------- Output No $i ----------" >> output.txt
+			sleep 1
+			let i+=1
+		done
+		owner 
+}
+output
